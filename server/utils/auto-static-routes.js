20fddae9d54a2195a319ef263b6e5ec75160d96d
Matthew Hall <mhall@bouncingpixel.com>
2017-01-26 11:55:24 -0600

    finish auto routes for faster mode on production

--- server/utils/auto-static-routes.js
+++ server/utils/auto-static-routes.js
@@ -1,14 +1,61 @@
+const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const logger = require('winston');
+
+const renderStaticPage = require('./render-static-page');
+
+const baseViewsDir = path.resolve(__dirname, '..', '..', 'views');
 
 // this one is used in production
 function createAllStaticRoutes() {
   // create an express router
-  // go through list of files
-  // add routes for each of them
+  const router = express.Router();
+
+  // go through list of files and add routes for each of them
+  const staticDir = path.join(baseViewsDir, 'static');
+  addRoutesInDir(staticDir, '/', router);
 
   // we return the router, even though this is async and the router is filled later
   // express is fine with this
+  return router;
+}
+
+// no callback as it doesn't really matter. we don't notify anything that we are done
+function addRoutesInDir(baseDir, dir, router) {
+  const fullDir = path.join(baseDir, dir);
+
+  fs.stat(fullDir, function(err, stats) {
+    if (err) {
+      logger.warn(err);
+      return;
+    }
+
+    if (stats.isDirectory()) {
+      fs.readdir(fullDir, function(err, files) {
+        if (err) {
+          logger.warn(err);
+          return;
+        }
+
+        files.forEach(function(file) {
+          addRoutesInDir(baseDir, path.join(dir, file), router);
+        });
+      });
+    } else {
+      // make sure it ends in .dust
+      if (dir.lastIndexOf('.dust') !== (dir.length - 5)) {
+        return;
+      }
+
+      // if it is index, then convert that to /
+      if (dir.lastIndexOf('/index.dust') === (dir.length - 11)) {
+        dir = dir.substr(0, dir.length - 10);
+      }
+
+      router.get(dir, renderStaticPage(path.join('static', dir)));
+    }
+  });
 }
 
 // this one is used in dev
@@ -16,7 +63,7 @@ function createStaticHandler() {
   return function(req, res, next) {
     // also, make sure if it ends in a slash, then use index.dust
     const urlpath = 'static/' + req.path.substr(1) + (req.path[req.path.length-1] === '/' ? 'index' : '');
-    const pagePath = path.resolve(__dirname, '..', '..', 'views', urlpath + '.dust');
+    const pagePath = path.resolve(baseViewsDir, urlpath + '.dust');
 
     // security: make sure someone doesnt navigate out of the top folder with urlpath
     if (urlpath.indexOf('/./') !== -1 || urlpath.indexOf('/../') !== -1) {
@@ -39,5 +86,4 @@ function createStaticHandler() {
   };
 }
 
-//module.exports = process.env.NODE_ENV === 'production' ? createAllStaticRoutes : createStaticHandler;
-module.exports = createStaticHandler;
+module.exports = process.env.NODE_ENV === 'production' ? createAllStaticRoutes : createStaticHandler;
