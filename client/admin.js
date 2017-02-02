2af32fbb458bf00d55487241ae03eaecf2a7171c
Matthew Hall <mhall@bouncingpixel.com>
2017-02-02 10:58:33 -0600

    add trumbowg wysiwyg editor

--- client/admin.js
+++ client/admin.js
@@ -2,3 +2,9 @@ const $ = require('jquery');
 
 $('.button-collapse').sideNav({'edge': 'left'});
 $('select').not('.disabled').material_select();
+
+$.trumbowyg.svgPath = '/images/trumbowyg-icons.svg';
+
+$('.wyssimpleeditor').trumbowyg({
+  btns: [['bold', 'italic'], ['link']]
+});
