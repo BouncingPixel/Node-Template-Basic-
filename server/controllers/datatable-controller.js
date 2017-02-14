18f45489de63c9a8c09610c0109663ccf5518b32
Matthew Hall <mhall@bouncingpixel.com>
2017-02-14 09:35:20 -0600

    fixing datatables controller

--- server/controllers/datatable-controller.js
+++ server/controllers/datatable-controller.js
@@ -5,13 +5,16 @@ module.exports = {
   // the function doesn't quite classify as a middleware, so I picked making it a controller
   makeHandler: function(Model) {
     return function(req, res) {
-      const columns = req.query.columns.reduce((c, col) => {
+      // why does this end up becoming one col of data? so weird
+      const selects = req.query.columns.reduce((c, col) => {
         c[col.data] = 1;
         return c;
-      });
+      }, {});
 
       const query = req.query.columns.reduce((q, col) => {
-        q[col.data] = col.search.value;
+        if (col.search.value && col.search.value.length) {
+          q[col.data] = col.search.value;
+        }
         return q;
       }, {});
 
@@ -21,11 +24,11 @@ module.exports = {
         return s;
       }, {});
 
-      const start = req.query.start;
-      const length = req.query.length;
+      const start = parseInt(req.query.start, 10) || 0;
+      const length = parseInt(req.query.length, 10);
 
       const totalPromise = Model.count({});
-      const filtered = Model.find(query).select(columns).sort(sorter).skip(start).limit(length).lean().exec();
+      const filtered = Model.find(query).select(selects).sort(sorter).skip(start).limit(length).lean();
 
       Promise.all([totalPromise, filtered]).then((results) => {
         const foundRecords = results[1];
